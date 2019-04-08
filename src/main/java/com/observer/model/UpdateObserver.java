a7e120ca54cb0d3e7b6f5227a1fecf8ca66f163a
doyewo <dexteroyewo@gmail.com>
2019-04-07 22:49:52 -0500

    "UpdateObserver"

--- src/main/java/com/observer/model/UpdateObserver.java
+++ src/main/java/com/observer/model/UpdateObserver.java
@@ -2,5 +2,20 @@ package com.observer.model;
 
 public class UpdateObserver implements Observer {
 
+    private int observerState;
 
+    private UpdateSubject subject;
+
+    public UpdateObserver(UpdateSubject subject) {
+        this.subject = subject;
+    }
+
+    public void update() {
+        observerState = subject.getState();
+
+    }
+
+    protected int getObserverState() {
+        return observerState;
+    }
 }
